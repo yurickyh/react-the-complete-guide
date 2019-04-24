26510b789f535c8614c779a1dd372b818ce9ce6b
yurickyh <yurickyussuke@hotmail.com>
2019-04-23 22:54:45 -0300

    Add and use Radium, also create an attribute hover to style variable.
    Signed-off-by: yurickyh <yurickyussuke@hotmail.com>

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Radium from 'radium';
 import Person from './Person/Person'
 
 class App extends Component{
@@ -50,6 +51,10 @@ class App extends Component{
       border: '1px solid blue',
       padding: '8px',
       cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black',
+      }
     }
 
     let persons = null;
@@ -78,6 +83,10 @@ class App extends Component{
         </div>
       );
       style.backgroundColor = 'red';
+      style[':hover'] = {
+        backgroundColor: 'salmon',
+        color: 'black',
+      }
     }
 
     let classes = [];
@@ -102,4 +111,4 @@ class App extends Component{
   }
 }
 
-export default App;
+export default Radium(App);
