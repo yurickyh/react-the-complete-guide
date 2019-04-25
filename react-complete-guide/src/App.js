990f7ba34a5d8297b8dbf4e754268849fa3c17c8
yurickyh <yurickyussuke@hotmail.com>
2019-04-24 21:17:51 -0300

    Use CSS Modules.
    Signed-off-by: yurickyh <yurickyussuke@hotmail.com>

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.css';
 import Person from './Person/Person'
 
 class App extends Component{
@@ -80,19 +80,19 @@ class App extends Component{
       style.backgroundColor = 'red';
     }
 
-    let classes = [];
+    let assignedClasses = [];
 
     if(this.state.persons.length <= 2) {
-      classes.push('red'); // classes = ['red']
+      assignedClasses.push(classes.red); // assignedClasses = ['red']
       if(this.state.persons.length <= 1) {
-        classes.push('bold'); // classes = ['red', 'bold']
+        assignedClasses.push(classes.bold); // assignedClasses = ['red', 'bold']
       }
     }
 
     return (
-      <div className="App">
+      <div className={classes.App}>
         <h1>Hi, I'm a React App</h1>
-        <p className={classes.join(' ')}>This is really working</p>
+        <p className={assignedClasses.join(' ')}>This is really working</p>
         <button style={style} onClick={this.togglePersonsHandler}>Toggle Persons</button>
         {persons}
       </div>
