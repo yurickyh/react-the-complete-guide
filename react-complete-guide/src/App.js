dc03d49d4e59cef92d7a966de612659ae80d28b3
yurickyh <yurickyussuke@hotmail.com>
2019-04-24 21:34:07 -0300

    Add style using CSS Modules on the button.
    Signed-off-by: yurickyh <yurickyussuke@hotmail.com>

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -42,17 +42,8 @@ class App extends Component{
   }
 
   render() {
-
-    const style = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer',
-    }
-
     let persons = null;
+    let btnClass = '';
     if (this.state.showPersons) {
       persons = (
         <div>
@@ -77,7 +68,7 @@ class App extends Component{
             age={this.state.persons[2].age}/> */}
         </div>
       );
-      style.backgroundColor = 'red';
+      btnClass = classes.Red;
     }
 
     let assignedClasses = [];
@@ -93,7 +84,7 @@ class App extends Component{
       <div className={classes.App}>
         <h1>Hi, I'm a React App</h1>
         <p className={assignedClasses.join(' ')}>This is really working</p>
-        <button style={style} onClick={this.togglePersonsHandler}>Toggle Persons</button>
+        <button className={btnClass} onClick={this.togglePersonsHandler}>Toggle Persons</button>
         {persons}
       </div>
       //<button onClick={() => this.switchNameHandler('Yurick Yussuke Honda')}>Switch name</button>
